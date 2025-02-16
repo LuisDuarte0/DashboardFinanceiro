c9e8067c932893ec3d67804eddd183e10e5d86f6
Lyuiss <luiscmduarte077@gmail.com>
2025-02-16 17:10:47 -0300

    Área Dashboard das Ações concluída

--- obj/Debug/net9.0/DashboardFinanceiroXP.AssemblyInfo.cs
+++ obj/Debug/net9.0/DashboardFinanceiroXP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashboardFinanceiroXP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ac628258b46d371f6aaa1eb67633c42e5a1192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c41931383ea181af8ee5359074dcc6428d855f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashboardFinanceiroXP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashboardFinanceiroXP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
